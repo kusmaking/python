8afad8caa00fcc7902e930ba257c26ba56f2e87d
Kusmaking <olis.hansen@gmail.com>
2015-06-26 10:49:43 +0200

    played around with python
    added text for calculating days

--- my_code.py
+++ my_code.py
@@ -11,3 +11,20 @@ print word[4:]
 print word[:6]
 
 
+rass = 'tulling'
+
+
+print rass * 100
+
+print rass [3]
+
+print rass [-1]
+
+
+years_old = 38
+days_in_year = 365.25 + 345
+
+print years_old * days_in_year
+
+
+
