22733f515bc891e45846b6793bde2393a68c2ab3
Kusmaking <olis.hansen@gmail.com>
2015-07-03 13:08:57 +0200

    added some more examples and notes

--- my_code.py
+++ my_code.py
@@ -1,17 +1,13 @@
+def functionname( parameters ):
+   "function_docstring"
+   function_suite
+   return [expression]
 
 
+def square(n):
+   return n * n 
 
+p = 85
+print square (p)
 
-example = "Wow! Python is great! Don't you think?"
-
-
-print example
-print example.find('!')
-print example.find('!', first + 1)
-#first = example.find('!')
-#second = example.find('!', first + 1)
-#new_string = example[:first] + example[first+1:second] + example[second+1:]
-#print new_string # oops, I should probably replace the !s periods
-#new_string = example[:first] +'.'+ example[first+1:second] +'.'+ example[second+1:]
-#print new_string
 
