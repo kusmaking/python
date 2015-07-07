43478d4c8bfe28e12c75f8f586bf47da18395227
Kusmaking <olis.hansen@gmail.com>
2015-07-07 13:16:50 +0200

    updateded file

--- my_code.py
+++ my_code.py
@@ -7,7 +7,37 @@ def functionname( parameters ):
 def square(n):
    return n * n 
 
-p = 85
-print square (p)
+#p = 85
+print square (5)
 
 
+
+
+# Define a procedure, abbaize, that takes
+# two strings as its inputs, and returns
+# a string that is the first input,
+# followed by two repetitions of the second input,
+# followed by the first input.
+
+
+
+def abbaize (a,b):
+    return a + b + b + a
+
+
+print abbaize ('dog','cat')
+
+
+
+
+#print abbaize('a','b')
+#>>> 'abba'
+
+#print abbaize('dog','cat')
+#>>> 'dogcatcatdog'
+
+def sum (a,b):
+	a = a + b
+	return a
+
+print sum (2,123)
